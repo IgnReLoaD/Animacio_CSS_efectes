e199b32486a8c276652bf2a5a54aff7a198c7963
IgnReLoaD <ortiz.ignasi@gmail.com>
2021-01-31 17:33:38 +0100

    deformació pilota restaurada després de golpejar. Incorpora JS per pausar i veure la forma.

---
+++ events.js
@@ -0,0 +1,8 @@
+function pausa() { 
+    console.log("El botó 'Pausar' ha estat pulsat.");
+    alert("El botó 'Pausar' ha estat pulsat.");
+}
+
+// window.addEventListener('keydown', function (e) {
+//    alert (key);
+// }); -->
